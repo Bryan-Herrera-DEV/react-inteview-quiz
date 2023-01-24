1c65bb8650cd88639b4497193ab8e73ef8cd377f
Bryan Herrera ~ ርᚱ1ናተᛰ ᚻህᚥተპᚱ <50712646+Bryan-Herrera-DEV@users.noreply.github.com>
2023-01-23 21:47:21 -0500

    [:lipstick: style] configure style basic system and set init landing design

---
+++ src/shared/utils/styles.ts
@@ -0,0 +1,10 @@
+import { IStyles } from "../interfaces/styles.interface";
+
+export const styles: IStyles = {
+  boxWidth: "xl:max-w-[1280px] w-full",
+
+  paddingX: 'sm:px-16 px-6',
+
+  flexCenter: "flex justify-center items-center",
+  flexStart: "flex justify-center items-start"
+};
--- src/views/landing/index.tsx
+++ src/views/landing/index.tsx
@@ -1,9 +1,25 @@
+import { styles } from '@/shared/utils/styles';
+
 const index = () => {
   return (
-    <div>
-      Test
-    </div>
-  )
-}
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>nav</div>
+      </div>
 
-export default index
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth} ${styles.paddingX}`}>hero</div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX}  ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          {/*
+            All our components
+          */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default index;
