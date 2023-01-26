a8c8267d8659534d7a184dc708e7db56d41d898f
Eliseo Arévalo <2480931@clases.edu.sv>
2023-01-25 18:31:56 -0600

    [:bug: fix] fix error in tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        primary: "#fff",
+        primary: "#00040f",
         secondary: "#00f6ff",
         dimWhite: "rgba(255, 255, 255, 0.7)",
         dimBlue: "rgba(9, 151, 124, 0.1)",
