568a418bdfa8765ac2ba9a439130d7b84e0d6f67
Bryan Herrera ~ ርᚱ1ናተᛰ ᚻህᚥተპᚱ <50712646+Bryan-Herrera-DEV@users.noreply.github.com>
2023-01-28 20:20:20 -0500

    [:sparkles:/:bug: feat/fix] fix error with isolatedModules and add istambul to coverage tests

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,11 @@ const config: Config.InitialOptions = {
   verbose: true,
   transform: {
     "^.+\\.tsx?$": "ts-jest"
-  }
+  },
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/tests/**',
+  ],
 };
 export default config;
